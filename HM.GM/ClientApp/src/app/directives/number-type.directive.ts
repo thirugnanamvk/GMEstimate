4f410170a3ce8bcde08e63c1de15c11faceb8118
Rahul Gourha <Rahul.Gourha@happiestminds.com>
2018-04-10 17:12:05 +0530

    allowed tab key

--- HM.GM/ClientApp/src/app/directives/number-type.directive.ts
+++ HM.GM/ClientApp/src/app/directives/number-type.directive.ts
@@ -15,8 +15,16 @@ export class NumberTypeDirective implements OnInit {
 
   @HostListener('keydown', ['$event'])
   private keyDown(evt: KeyboardEvent): void {
-    if ((evt.which < 48 || evt.which > 57) && evt.which != 8 && evt.which != 190 && evt.which != 46 && evt.which != 37 && evt.which != 38 && evt.which != 39 && evt.which != 40 ) {
-      console.log(evt.which);
+    if ((evt.which < 48 || evt.which > 57)
+      && evt.which != 8
+      && evt.which != 190
+      && evt.which != 46
+      && evt.which != 37
+      && evt.which != 38
+      && evt.which != 39
+      && evt.which != 40
+      && evt.which != 9) {
+      //console.log(evt.which);
       evt.preventDefault();
     }
   }
